1c59a484f8459329fc264bc07a40510be509c08d
JuyunLee <juyun9432@gmail.com>
2020-12-26 22:25:23 +0900

    typo

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ if($_SERVER['HTTPS'] != "on"){ ?>
 <!DOCTYPE html>
 <html>
   <head>
-    <title>Walk to alive</title>
+    <title>Walk to be alive</title>
     <meta charset="utf8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
